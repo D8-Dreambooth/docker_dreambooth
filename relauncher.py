c20839dfee822e7b1258423a9a36a93bb729ef7d
d8ahazard <d8ahazard@gmail.com>
2023-02-07 19:09:48 -0600

    Add flagz

--- relauncher.py
+++ relauncher.py
@@ -2,13 +2,16 @@ import os, time
 
 n = 0
 gradio_auth = os.getenv('GRADIO_AUTH')
+torch2 = os.getenv('TORCH2')
 while True:
     print('Relauncher: Launching...')
     if n > 0:
         print(f'\tRelaunch count: {n}')
-    launch_string = "python webui.py --port 3000 --xformers --ckpt-dir /workspace/models/ --listen --enable-insecure-extension-access --no-half --torch2"
+    launch_string = "python webui.py --port 3000 --xformers --ckpt-dir /workspace/models/ --listen --enable-insecure-extension-access --no-half"
     if gradio_auth:
         launch_string += " --gradio-auth " + gradio_auth
+    if torch2:
+        launch_string += " --torch2"
     os.system(launch_string)
     print('Relauncher: Process is ending. Relaunching in 2s...')
     n += 1
--- start.sh
+++ start.sh
@@ -7,11 +7,43 @@ cd /workspace/stable-diffusion-webui
 pip install -r /workspace/stable-diffusion-webui/requirements_versions.txt
 pip install -r /workspace/stable-diffusion-webui/extensions/sd_dreambooth_extension/requirements.txt
 
+if [[ $UPDATE_EXTENSION ]]
+then
+	cd /workspace/stable-diffusion-webui/extensions/sd_dreambooth_extension/
+	git fetch && git pull
+	cd /workspace/stable-diffusion-webui	
+fi
+
+if [[ $UPDATE_WEBUI ]]
+then
+	cd /workspace/stable-diffusion-webui/extensions/sd_dreambooth_extension/
+	git fetch && git pull
+fi
+
+cd /workspace/stable-diffusion-webui
+
 if [[ $TORCH2 ]]
 then
-	# Install Requirements
-	pip install https://download.pytorch.org/whl/nightly/cu118/torch-2.0.0.dev20230202%2Bcu118-cp310-cp310-linux_x86_64.whl https://download.pytorch.org/whl/nightly/cu118/torchvision-0.15.0.dev20230202%2Bcu118-cp310-cp310-linux_x86_64.whl https://download.pytorch.org/whl/nightly/pytorch_triton-2.0.0%2B0d7e753227-cp310-cp310-linux_x86_64.whl
+	# Get the current date in the format YYYYMMDD
+	current_date=$(date +%Y%m%d)
 
+	# Check if $TORCH_REVISION is set
+	if [ -z "$TORCH_REVISION" ]; then
+	  torch_revision=$current_date
+	else
+	  torch_revision=$TORCH_REVISION
+	fi
+
+	# Use the value of $torch_revision in the URL
+	torch_url="https://download.pytorch.org/whl/nightly/cu118/torch-2.0.0.dev${torch_revision}%2Bcu118-cp310-cp310-linux_x86_64.whl"
+	torch_vision_url="https://download.pytorch.org/whl/nightly/cu118/torchvision-0.15.0.dev{torch_revision}%2Bcu118-cp310-cp310-linux_x86_64.whl"
+	torch_triton_url="https://download.pytorch.org/whl/nightly/cu118/pytorch_triton-2.0.0%2B0d7e753227-cp310-cp310-linux_x86_64.whl"
+	# Install Requirements
+	pip install "$torch_url" "$torch_vision_url" "$torch_triton_url"
+	pip install ninja
+	# Set TORCH_CUDA_ARCH_LIST if running and building on different GPU types
+	export TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.2;7.5;8.0;8.6"
+	pip install -v -U git+https://github.com/facebookresearch/xformers.git@main#egg=xformers
 else
 	pip install torch==1.13.1+cu118 torchvision==0.14.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 	pip install xformers==0.0.17.dev442
